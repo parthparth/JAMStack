3b9575a066aa0d75d2931f41820c3329d3659d39
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-10-16 16:43:49 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>I'm Parth, a 15 year old, web developer from India. Have a happy October ! :)</p>
+  </Layout>
+)
+
+export default IndexPage
