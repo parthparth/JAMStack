c92da7a22dd26a88743be2215b8d70ad5a730c87
Parth Parth <16730815+parthparth@users.noreply.github.com>
2018-10-16 17:16:32 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>I'm Parth, a 15 year old, web developer from India. Have a happy October ! :)</p>
+    <h4> Thank you for the JAMStack HAck-a-thon ticket in advance. If I don't get it, well LOL ;)  Never mind</h4>
   </Layout>
 )
 
